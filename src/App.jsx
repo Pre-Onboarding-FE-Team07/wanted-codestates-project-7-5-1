84eb3152d4132bb616afbbf6fca8f729c16c0357
Lee-Ye-Ji <dpwl30559@gmail.com>
2022-02-24 21:16:39 +0900

    Fix: App.js merge 충돌 고치기

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
 import { useState, createContext } from 'react';
 import { Routes, Route } from 'react-router-dom';
-
 import WithHeaderPage from './pages/WithHeaderPage';
 import MainPage from './pages/MainPage';
-<<<<<<< HEAD
 import KeywordPage from './pages/KeywordPage';
-=======
-// import KeywordPage from './pages/KeywordPage';
->>>>>>> 4372d2313874e51460da47396a4350b1c26a5b69
 import ProductPage from './pages/ProductPage';
 
 export default function App() {
@@ -20,7 +15,7 @@ export default function App() {
         <Routes>
           <Route element={<WithHeaderPage />}>
             <Route path="/" element={<MainPage />} />
-            {/* <Route path="/keyword" element={<KeywordPage />} /> */}
+            <Route path="/keyword" element={<KeywordPage />} />
             <Route path="/product">
               <Route path=":product_code" element={<ProductPage />} />
             </Route>

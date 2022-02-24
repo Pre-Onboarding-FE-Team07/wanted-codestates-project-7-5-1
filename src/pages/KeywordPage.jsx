fe436308742072d592f1eb82d16c638df1a34bea
seomoon <52448114+dev-seomoon@users.noreply.github.com>
2022-02-25 06:20:57 +0900

    Hotfix: KeywordPage 버그 해결

--- src/pages/KeywordPage.jsx
+++ src/pages/KeywordPage.jsx
@@ -3,16 +3,17 @@ import { useNavigate } from '../../node_modules/react-router/index';
 import { SearchResultContext } from '../App';
 import Card from '../components/Card';
 import ErrorMessage from '../components/ErrorMessage';
+import useSearch from '../hooks/useSearch';
 
 export default function KeywordPage() {
   const { searchResult } = useContext(SearchResultContext);
+  const { search } = useSearch();
   const navigate = useNavigate();
-
   const handleErrorConfirm = () => {
     navigate('/');
   };
 
-  if (!searchResult || !searchResult.length)
+  if (!searchResult && !searchResult.length)
     return (
       <ErrorMessage
         text="존재하지 않는 상품입니다. "
@@ -21,22 +22,20 @@ export default function KeywordPage() {
     );
   else
     return (
-      <div>
-        <div className="bg-white">
-          <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-            <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-              {searchResult.map((product) => (
-                <div key={product.product_code} className="group relative">
-                  <Card
-                    name={product.name}
-                    price={product.price}
-                    imageUrl={product.image_url}
-                  />
-                </div>
-              ))}
-            </div>
-          </div>
+      <section className="mx-auto box-border flex w-full p-10">
+        <div className="flex flex-wrap justify-center">
+          {searchResult.map((product) => (
+            <Card
+              key={product.product_code}
+              name={product.name}
+              price={product.price}
+              imageUrl={product.image_url}
+              onClickEvent={() => {
+                search(product.image_url);
+              }}
+            />
+          ))}
         </div>
-      </div>
+      </section>
     );
 }

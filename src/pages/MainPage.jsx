66cc7d6d68cbd5941ffd02567a8d8376d52b41ac
Lee-Ye-Ji <dpwl30559@gmail.com>
2022-02-24 21:30:27 +0900

    Fix: MainPage 충돌 난 부분 고치기

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,19 +1,13 @@
-<<<<<<< HEAD
-export default function MainPage() {
-  return <div>MainPage</div>;
-}
-=======
-import SearchBar from "../components/Main/SearchBar";
-import Title from "../components/Main/Title";
+import SearchBar from '../components/Main/SearchBar';
+import Title from '../components/Main/Title';
 
-const MainPage = () =>{
-    return (
-        <div className="Home m-auto text-center w-[90vw] max-w-[800px] h-fit mt-[14vh]">
-            <Title/>
-            <SearchBar/>
-        </div>
-    );
-}
+const MainPage = () => {
+  return (
+    <div className="Home m-auto mt-[14vh] h-fit w-[90vw] max-w-[800px] text-center">
+      <Title />
+      <SearchBar />
+    </div>
+  );
+};
 
 export default MainPage;
->>>>>>> 4372d2313874e51460da47396a4350b1c26a5b69

88434ebd43f72bf5ba3f81e60c8636d77fef3646
ye-yo <yeyo0x0@gmail.com>
2022-02-24 20:37:21 +0900

    Feat: 데이터 필터링 및 라우팅
    검색어 판별하여 검색결과 필터링 후, 해당하는 경로로 이동하도록 만들었습니다.
    
    Related to #1

--- src/components/Main/SearchBar.jsx
+++ src/components/Main/SearchBar.jsx
@@ -1,17 +1,70 @@
+import { useState, useContext, useCallback } from "react";
+import { useNavigate } from 'react-router-dom';
+import { SearchResultContext, RecomandListContext } from '../../App.jsx';
+import products from "../../datas/products.json";
+
+const checkUrl = /^http[s]?\:\/\//i;
+function getWordType(value) {
+    if (isNaN(value)) {
+        if (checkUrl.test(value))
+            return 'image_url'
+        else return 'name';
+    }
+    return 'product_code';
+}
+
+function getFilteredData(word, wordType) {
+    return products.filter((product) => wordType === 'image_url' ? product.includes(word) : product[wordType] == word)
+}
+
 const SearchBar = () => {
-    const handleClick = () =>{
+    const [searchKeyword, setSearchKeyword] = useState();
+    const [isEmptyResult, setIsEmptyResult] = useState(false);
+    const navigate = useNavigate();
+    const searchResultDispatch = useContext(SearchResultContext);
+    const recommendListDispatch = useContext(RecomandListContext);
+
+    const handleCloseModal = useCallback((e) => {
+        setIsEmptyResult(false);
+    }, []);
+
+    const handleSearchClick = (e) => {
+        if (searchKeyword.replace(/\s/gi, "") !== "") {
+            const word = searchKeyword.trim();
+            const wordType = getWordType(word);
+            const filteredProducts = getFilteredData(word, wordType);
+            
+            searchResultDispatch.setSearchResult(filteredProducts);
+            if (wordType === 'name') {
+                navigate('/keyword');
+            }
+            else {
+                recommendListDispatch.setRecommendList(filteredProducts);
+                navigate(`/product/:${word}`);
+            }
+        }
     }
 
-    const handleInputChange = (e) => {
-    }
+    const handleKeyUp = useCallback((e) => {
+        if (e.keyCode === 13) {
+            handleSearchClick();
+        }
+    }, [searchKeyword]);
+
+    const handleInputChange = useCallback((e) => {
+        setSearchKeyword(e.target.value);
+    }, []);
+
+
     return (
         <div className="flex flex-row justify-between">
             <input type="text" placeholder="IMAGE URL or KEYWORD"
                 className="shadow-[0_4px_20px_-10px_rgba(0,0,0,0.3)] rounded-full px-8 py-4 text-left flex-1 focus:outline-0 focus:shadow-[0_4px_20px_-10px_rgba(0,0,0,0.5)]"
                 onChange={handleInputChange}
+                onKeyUp={handleKeyUp}
             />
             <button className="rounded-md w-14 md:w-20 my-2 text-zinc-500 bg-gray-200 ml-2 md:ml-10 tracking-wider hover:bg-gray-300"
-                onClick={handleClick}>검색</button>
+                onClick={handleSearchClick}>검색</button>
         </div>
     );
 };

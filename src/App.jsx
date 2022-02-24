62b8b94de36d3c55eda429e1122b17dfc9589fb9
Charlie_egpu <tchoi.tab@gmail.com>
2022-02-24 16:35:54 +0900

    :gear: wip: 기본 라우팅, 헤더, 스테이트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,39 @@
+import { useState, createContext } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import WithHeaderPage from './pages/WithHeaderPage';
+import MainPage from '.pages/MainPage';
+import KeywordPage from './pages/KeywordPage';
+import ProductPage from './pages/ProductPage';
+
 export default function App() {
-  return <div className="bg-yellow-500 font-bold">Hello</div>;
+  const [searchResult, setSearchResult] = useState([]);
+  const [recommendList, setRecommendList] = useState([]);
+
+  return (
+    <RecomandListContext.Provider value={{ recommendList, setRecommendList }}>
+      <SearchResultContext.Provider value={{ searchResult, setSearchResult }}>
+        <Routes>
+          <Route element={<WithHeaderPage />}>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/keyword" element={<KeywordPage />} />
+            <Route path="/product">
+              <Route path=":product_code" element={<ProductPage />} />
+            </Route>
+          </Route>
+        </Routes>
+      </SearchResultContext.Provider>
+    </RecomandListContext.Provider>
+  );
 }
+
+//Contexts
+export const SearchResultContext = createContext({
+  searchResult: [],
+  setSearchResult: () => {},
+});
+
+export const RecomandListContext = createContext({
+  recommendList: [],
+  setRecomendList: () => {},
+});

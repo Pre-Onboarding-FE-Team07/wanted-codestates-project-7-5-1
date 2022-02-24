a547f8adcb6385f4dd376da7bd7cd9e52ff9a8fc
from97 <skm1104@daum.net>
2022-02-24 21:19:58 +0900

    Feat: Product Page context 구독 및 에러 핸들링

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,10 +1,29 @@
+import { useContext } from 'react';
+import { useNavigate } from '../../node_modules/react-router/index';
+import { SearchResultContext } from '../App';
+import ErrorMessage from '../components/ErrorMessage';
 import Aside from '../components/Product/Aside';
+import Recommends from '../components/Product/Recommends';
 
 const ProductPage = () => {
+  const { searchResult } = useContext(SearchResultContext);
+  const navigate = useNavigate();
+
+  const handleErrorConfirm = () => {
+    navigate('/');
+  };
+  if (!searchResult || !searchResult.length)
+    return (
+      <ErrorMessage
+        text="존재하지 않는 상품입니다. "
+        onClickEvent={handleErrorConfirm}
+      />
+    );
+
   return (
     <article className="flex flex-row">
       <Aside />
-      <div className="w-full py-4">Right Section</div>
+      <Recommends />
     </article>
   );
 };

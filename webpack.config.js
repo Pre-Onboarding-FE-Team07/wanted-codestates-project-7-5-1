d4e9992ec0ca12908f3be34964b02bbc2bf6a188
HyoEun Ye <50618754+ye-yo@users.noreply.github.com>
2022-02-25 02:15:37 +0900

    Refactor: Search 로직 커스텀 훅으로 분리 (#24)
    * Feat: 데이터 필터링 및 라우팅
    
    검색어 판별하여 검색결과 필터링 후, 해당하는 경로로 이동하도록 만들었습니다.
    
    Related to #1
    
    * Feat: 결과 없음 팝업창 적용
    
    검색 결과 없을 시 에러메시지 뜨도록 만들었습니다.
    
    Realted to #1
    
    * Fix: 검색 기능 오류 수정
    
    추천 상품 목록 필터링 추가 및 이미지 url 인코딩
    
    Related to #1
    
    * Fix: 인코딩 방식 변경
    
    btoa > encodeURIComponent
    
    Related to #1
    
    * Refator: 함수 작성방식 변경
    
    * Chore: git cache 제거
    
    * Fix: gitignore 추가
    
    * Refactor: 에러메시지창 제거 및 리팩토링
    
    * Fix: searchResult 전달 데이터 수정
    
    * Chore: 빈 라인 제거
    
    * Fix: 데이터 가져오는 방식 변경
    
    local file > http 요청으로 변경
    
    Related to #1
    
    * Chore: env 관련 모듈 설치
    
    * Chore: 검색 필터링 기능 분리
    
    검색 필터링 기능을 훅으로 분리했습니다.
    
    Related to #1
    
    Co-authored-by: seomoon <52448114+dev-seomoon@users.noreply.github.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-
+const webpack = require("webpack");
+const dotenv = require("dotenv");
+dotenv.config();
+ 
 module.exports = {
   mode: 'development',
   devtool: 'cheap-eval-source-map',
@@ -61,6 +64,9 @@ module.exports = {
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './public/index.html',
+    }),
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(process.env), 
     }),
   ],
 };

8bab338cfbeb8ba688ffbdf5a20d4b07cfa8b8a8
Lela12 <dbcocotn4@hanmail.net>
2022-02-25 13:03:19 +0900

    keywordPage code change

--- src/pages/KeywordPage.jsx
+++ src/pages/KeywordPage.jsx
@@ -3,12 +3,12 @@ import { useNavigate } from '../../node_modules/react-router/index';
 import { SearchResultContext } from '../App';
 import Card from '../components/Card';
 import ErrorMessage from '../components/ErrorMessage';
-import useSearch from '../hooks/useSearch';
 import Loading from '../components/Loading';
+import useSearch from '../hooks/useSearch';
 
 export default function KeywordPage() {
-  const [loading, setLoading] = useState(true);
   const { searchResult } = useContext(SearchResultContext);
+  const [loading, setLoading] = useState(true);
   const { search } = useSearch();
   const navigate = useNavigate();
   const handleErrorConfirm = () => {
@@ -16,10 +16,14 @@ export default function KeywordPage() {
   };
 
   useEffect(() => {
-    setLoading(false);
-  });
+    if (searchResult) {
+      setLoading(false);
+    }
+  }, []);
 
-  if (!searchResult && !searchResult.length)
+  if (loading) {
+    return <Loading />;
+  } else if (!searchResult || !searchResult.length)
     return (
       <ErrorMessage
         text="존재하지 않는 상품입니다. "
@@ -28,26 +32,20 @@ export default function KeywordPage() {
     );
   else
     return (
-      <div>
-        {loading ? (
-          <Loading />
-        ) : (
-          <div className="bg-white">
-            <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-              <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                {searchResult.map((product) => (
-                  <div key={product.product_code} className="group relative">
-                    <Card
-                      name={product.name}
-                      price={product.price}
-                      imageUrl={product.image_url}
-                    />
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-        )}
-      </div>
+      <section className="mx-auto box-border flex w-full p-10">
+        <div className="flex flex-wrap justify-center">
+          {searchResult.map((product) => (
+            <Card
+              key={product.product_code}
+              name={product.name}
+              price={product.price}
+              imageUrl={product.image_url}
+              onClickEvent={() => {
+                search(product.image_url);
+              }}
+            />
+          ))}
+        </div>
+      </section>
     );
 }
